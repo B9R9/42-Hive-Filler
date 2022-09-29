24717ff3ad4acafc1a0cde988a7620bc8fde0799
Baptiste Riffard <briffard@c1r1p2.hive.fi>
2022-09-29 15:46:29 +0300

    Merge to main hmap

--- run_player.sh
+++ run_player.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./resources/filler_vm -p1 ./$1 -p2 ./resources/players/$2 -f ./resources/maps/map0$3 | python3 vis.py
+./resources/filler_vm -p1 ./briffard.filler -p2 ./resources/players/carli.filler -f ./resources/maps/map02 | python3 vis.py
